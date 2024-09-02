29f83641bb68108039aa56ea7ef3d97e4706d26a
root <dwivedi.vinit310@gmail.com>
2024-09-02 06:15:46 +0000

    vinit new1

--- starter/frontend/src/components/MovieList.js
+++ starter/frontend/src/components/MovieList.js
@@ -4,42 +4,17 @@ import axios from 'axios';
 
 function MovieList({ onMovieClick }) {
   const [movies, setMovies] = useState([]);
-  const [loading, setLoading] = useState(true); // Loading state
-  const [error, setError] = useState(null); // Error state
 
   useEffect(() => {
-    const fetchMovies = async () => {
-      try {
-        const response = await axios.get(`${process.env.REACT_APP_MOVIE_API_URL}/movies`);
-        setMovies(response.data.movies);
-      } catch (err) {
-        setError(err);
-      } finally {
-        setLoading(false);
-      }
-    };
-
-    fetchMovies();
-
-    // Cleanup function to avoid updating state if the component unmounts
-    return () => {
-      setMovies([]);
-      setLoading(false);
-      setError(null);
-    };
+    axios.get(`${process.env.REACT_APP_MOVIE_API_URL}/movies`).then((response) => {
+      setMovies(response.data.movies);
+    });
   }, []);
-
-  if (loading) return <p>Loading...</p>;
-  if (error) return <p>Error fetching movies: {error.message}</p>;
 
   return (
     <ul>
       {movies.map((movie) => (
-        <li
-          className="movieItem"
-          key={movie.id}
-          onClick={() => onMovieClick(movie)}
-        >
+        <li className="movieItem" key={movie.id} onClick={() => onMovieClick(movie)}>
           {movie.title}
         </li>
       ))}
